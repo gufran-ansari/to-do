4e27e5468ebb5a21fe8605ce6c7528dc6d0f7241
gufran-ansari <gufzansari.ga@gmail.com>
2021-11-14 21:28:24 +0530

    to-do app

---
+++ todo/tasks/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import redirect, render
+from .forms import TaskForm
+from .models import *
+
+# Create your views here.
+
+
+def home(request):
+    tasks = Task.objects.all()
+
+    form = TaskForm()
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect("/")
+
+    context = {
+        'tasks': tasks,
+        'form': form
+    }
+    return render(request, 'tasks/create.html', context)
+
+
+def updateTask(request, pk):
+    task = Task.objects.get(id=pk)
+    form = TaskForm(instance=task)
+
+    if request.method == "POST":
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect("/")
+
+    context = {'form': form}
+
+    return render(request, 'tasks/update.html', context)
+
+
+def deleteTask(request, pk):
+    item = Task.objects.get(id=pk)
+
+    if request.method == "POST":
+        item.delete()
+        return redirect("/")
+
+    context = {'item': item}
+    return render(request, 'tasks/delete.html', context)
